bfe20336c3146312c0c9354803dad704ce6d492b
Itamar Axelrad <itamara@aman.co.il>
2022-04-17 21:57:52 +0300

    Added config object to Stack Navigator

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,16 @@ import HomeScreen from './screens/HomeScreen';
 interface IProps {}
 
 const Stack = createNativeStackNavigator();
+const screenOptions = {
+  headerShown: false,
+};
 
 const App: FC<IProps> = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        initialRouteName={routes.HOME}
+        screenOptions={screenOptions}>
         <Stack.Screen name={routes.HOME} component={HomeScreen} />
         <Stack.Screen name={routes.GAME} component={GameScreen} />
         <Stack.Screen name={routes.HIGH_SCORES} component={HighScoreScreen} />
