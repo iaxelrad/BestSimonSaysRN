15864a4a69c8697e6696aa0c94c7d422e5d6fb32
Itamar Axelrad <itamara@aman.co.il>
2022-04-17 19:22:33 +0300

    Added Custom Button

---
+++ src/shared/CustomButton.tsx
@@ -0,0 +1,41 @@
+import React, {FC} from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextStyle,
+  TouchableOpacity,
+  ViewStyle,
+} from 'react-native';
+
+interface ButtonProps {
+  onPress: () => void;
+  style?: ViewStyle;
+  buttonText: string;
+  textStyle?: TextStyle;
+}
+
+export const CustomButton: FC<ButtonProps> = ({
+  onPress,
+  style,
+  textStyle,
+  buttonText,
+}) => {
+  return (
+    <TouchableOpacity style={[styles.button, style]} onPress={onPress}>
+      <Text style={[styles.text, textStyle]}>{buttonText}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    width: '80%',
+    paddingVertical: 10,
+    borderRadius: 8,
+    marginVertical: 16,
+  },
+  text: {
+    fontSize: 16,
+    textAlign: 'center',
+  },
+});
