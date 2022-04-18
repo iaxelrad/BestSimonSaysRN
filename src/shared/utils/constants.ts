16e1489af8538a0ea2f892f4c7565d6d812663ee
Itamar Axelrad <itamara@aman.co.il>
2022-04-18 18:57:55 +0300

    Changed color format to hsl & added GAME_SPEED

--- src/shared/utils/constants.ts
+++ src/shared/utils/constants.ts
@@ -1,11 +1,16 @@
+import Sound from 'react-native-sound';
+
 export interface GameButtonModel {
   color: string;
+  sound: Sound;
 }
 export const gameButtons: GameButtonModel[] = [
-  {color: 'green'},
-  {color: 'red'},
-  {color: 'yellow'},
-  {color: 'blue'},
+  {color: '120', sound: new Sound('do_sound.mp3', Sound.MAIN_BUNDLE)},
+  {color: '0', sound: new Sound('re_sound.mp3', Sound.MAIN_BUNDLE)},
+  {color: '55', sound: new Sound('mi_sound.mp3', Sound.MAIN_BUNDLE)},
+  {color: '240', sound: new Sound('fa_sound.mp3', Sound.MAIN_BUNDLE)},
 ];
 
 export const MAX_NUM_OF_RESULTS = 10;
+
+export const GAME_SPEED = 15;
