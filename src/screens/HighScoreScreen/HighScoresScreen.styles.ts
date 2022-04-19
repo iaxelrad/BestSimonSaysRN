ee9f17341a3fa07a10e8a0951ac1e95e0a6c071e
Itamar Axelrad <itamara@aman.co.il>
2022-04-19 10:25:33 +0300

    Set High Score screen finctionailty

---
+++ src/screens/HighScoreScreen/HighScoresScreen.styles.ts
@@ -0,0 +1,42 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingVertical: 16,
+    marginHorizontal: 32,
+    backgroundColor: '#f1f1f1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonContainer: {flex: 1},
+  homeButton: {
+    backgroundColor: 'green',
+  },
+  homeButtonText: {
+    color: 'white',
+  },
+  background: {
+    alignItems: undefined,
+  },
+  title: {
+    flex: 5,
+    marginBottom: 16,
+  },
+  scoresContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  scoresTitle: {
+    textAlign: 'center',
+    marginBottom: 16,
+    fontSize: 30,
+    fontWeight: '600',
+  },
+  scoresTextInfo: {
+    fontSize: 22,
+    marginVertical: 16,
+    textAlign: 'center',
+    // flex: 1,
+  },
+});
--- src/screens/HighScoreScreen/index.tsx
+++ src/screens/HighScoreScreen/index.tsx
@@ -1,21 +1,62 @@
 import {useNavigation} from '@react-navigation/native';
-import React, {FC} from 'react';
-import {SafeAreaView, StyleSheet, View} from 'react-native';
+import React, {FC, useEffect, useState} from 'react';
+import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {routes} from '../../routes';
 import {CustomButton} from '../../shared/components/CustomButton';
 import {Header} from '../../shared/components/Header';
+import {IHighScore} from '../../shared/interfaces';
 import {Nav} from '../../shared/types';
+import {MAX_NUM_OF_RESULTS} from '../../shared/utils/constants';
+import {getHighScores, sortScores} from '../../shared/utils/helpers';
 
 interface IProps {}
 
 const HighScoreScreen: FC<IProps> = () => {
+  const [highScores, setHighScores] = useState<IHighScore[]>([]);
   const navigation = useNavigation<Nav>();
   const goToHome = () => {
     navigation?.popToTop();
   };
+
+  useEffect(() => {
+    const getScores = async () => {
+      const scores = await getHighScores();
+      setHighScores(scores);
+    };
+    getScores();
+  }, [highScores]);
+
+  const mapScores = (
+    {name, score}: {name: string; score: number},
+    index: number,
+  ) => {
+    return [
+      index > 0 && <View key={`${name}-separator`} style={styles.separator} />,
+      <Text key={name} style={styles.score}>{`${
+        index + 1
+      }. ${name} - score: ${score}`}</Text>,
+    ];
+  };
+
+  const renderContent = () => (
+    <ScrollView>
+      {highScores.sort(sortScores).slice(0, MAX_NUM_OF_RESULTS).map(mapScores)}
+    </ScrollView>
+  );
+
   return (
     <SafeAreaView style={styles.container}>
       <Header title="Top Scores" />
+      {renderContent()}
       <View style={styles.buttonContainer}>
+        <CustomButton
+          onPress={() => {
+            navigation.navigate(routes.GAME);
+          }}
+          buttonText="New Game"
+          style={styles.homeButton}
+          textStyle={styles.homeButtonText}
+        />
         <CustomButton
           onPress={goToHome}
           buttonText="Home Page"
@@ -45,4 +86,9 @@ const styles = StyleSheet.create({
   homeButtonText: {
     color: 'white',
   },
+  separator: {
+    height: 2,
+    backgroundColor: 'black',
+  },
+  score: {paddingVertical: 10, fontSize: 20},
 });
