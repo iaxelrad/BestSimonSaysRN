2bff9c8a3e65e559f3399818215cb0b226a2f864
Itamar Axelrad <itamara@aman.co.il>
2022-04-19 10:24:12 +0300

    Added AsyncStorage getter & setter funcs

--- src/shared/utils/helpers/index.ts
+++ src/shared/utils/helpers/index.ts
@@ -1,3 +1,6 @@
+import {IHighScore} from '../../interfaces';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 export const sleep = (time: number) =>
   new Promise(resolve => setTimeout(resolve, time));
 
@@ -6,5 +9,22 @@ export const getRandomInt = (min: number, max: number) => {
   return Math.floor(Math.random() * (Math.floor(max) - _min + 1)) + _min;
 };
 
-export const sortScores = (a: {score: number}, b: {score: number}) =>
-  b.score - a.score;
+export const sortScores = (a: IHighScore, b: IHighScore) => b.score - a.score;
+
+export const getHighScores = async () => {
+  try {
+    const jsonValue = await AsyncStorage.getItem('High Scores');
+    return jsonValue != null ? JSON.parse(jsonValue) : [];
+  } catch (e) {
+    console.log('Error reading high scores', e);
+  }
+};
+
+export const setHighScores = async (value: IHighScore[]) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem('High Scores', jsonValue);
+  } catch (e) {
+    console.log('Error setting high scores', e);
+  }
+};
